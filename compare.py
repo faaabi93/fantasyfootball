cf904f1958d13894476f60b1d4bd5b96d231d779
faaabi93 <58975284+faaabi93@users.noreply.github.com>
2020-01-09 16:35:12 +0100

    Update compare.py

--- compare.py
+++ compare.py
@@ -3,7 +3,7 @@ from os import path
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-DATA_DIR = "/Users/fabian.baiersdoerfer/Desktop/ff_data/raw"
+DATA_DIR = ""
 
 results2017 = pd.read_csv(path.join(DATA_DIR, "2017_season_results.csv"), sep=";")
 owner2017 = pd.read_csv(path.join(DATA_DIR, "2017_season_owners.csv"), sep=";")
@@ -11,10 +11,46 @@ owner2017 = pd.read_csv(path.join(DATA_DIR, "2017_season_owners.csv"), sep=";")
 results1 = results2017[["Week", "Type", "TeamA", "TeamAScore"]]
 results2 = results2017[["Week", "Type", "TeamB", "TeamBScore"]]
 
-print("Gib' die Namen der beiden Teams ein, die du vergleichen willst!")
-input1 = input("Team 1: ")
-input2 = input("Team 2: ")
+availableSeasons = [2017, 2018, 2019]
+season = 0
 
+while True:
+  try:
+    season = int(input("Welche Saison möchtest du analysieren? "))
+    if season in availableSeasons:
+      print("Du hast die Saison " + str(season) + " gewählt")
+      break
+    else:
+      print("Diese Saison ist nicht verfügbar!")
+  except ValueError:
+    print("Das ist kein zulässiges Format. Gib einfach nur ein Jahr ein. Beispielsweise '2017'")
+
+print("Welche Teams möchtest du vergleichen?")
+
+a = {}
+k = 0
+if season == 2017:
+  b = 10
+  print("Maximal 10 Teams möglich")
+else:
+  b = 12
+  print("Maximal 12 Teams möglich")
+
+print("Geben Sie die zu vergleichenden Teamnamen ein. stop für break")
+
+while k < b:
+  key = k
+  value = input("Teamnamen für das " + str(k+1) + ". Team eingeben ")
+  if value == "stop":
+    break
+  else:
+    a[key] = value 
+    k += 1
+
+for k, v in a.items():
+    print(k, v)
+
+locals().update(a)
 
 def winner(row):
     if row["TeamAScore"] > row["TeamBScore"]:
@@ -23,7 +59,6 @@ def winner(row):
         return row["TeamB"]
     else:
         return "draw"
-
 
 def teams(row):
     return [row["TeamA"], row["TeamB"]]
@@ -73,21 +108,35 @@ complete = pd.concat([teamA, teamB], sort=False)
 complete = complete.sort_values(by=["week"])
 complete["winner"] = complete.apply(set_winner, axis=1)
 
+a_list = list(a.values())
+output_list = []
+
+for i in a_list:
+  output = complete.query("team == @i").copy()
+  output["opponentScore"] = output.apply(get_opponent_score, axis=1)
+  with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    print(output)
+  output_list.append(output)
+
+print(output_list)
+"""
 # copy to avoid getting the 'SettingwithCopyWarning' error
-output = complete.query("team == @input1").copy()
+output = complete.query("team == @a[0]").copy()
 output["opponentScore"] = output.apply(get_opponent_score, axis=1)
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):
     print(output)
 
-output2 = complete.query("team == @input2").copy()
+output2 = complete.query("team == @a[1]").copy()
 output2["opponentScore"] = output2.apply(get_opponent_score, axis=1)
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):
     print(output2)
+"""
 
-comparison = pd.concat([output, output2], sort=False)
+comparison = pd.concat(output_list, sort=False)
 
 # Array of specific colors I later want to use in the hue in the replot-function
-colors = ["#4374B3", "#FF0B04"]
+colors = ["#4374B3", "#FF0B04", "#3CB44B", "#FFE119", "#FF5733", "#911EB4", "#42D4F4",
+"#F032E6", "#BFEF45", "#469990", "#000000"]
 # Setting the custom colorpalette
 sns.set_palette(sns.color_palette(colors))
 
@@ -99,4 +148,5 @@ plt.axvline(14, 0, 200, **keyword)
 # Set x and y limit to the graph
 g.set(ylim=(0, 200))
 g.set(xlim=(0, 16))
+g.savefig("test.png")
 plt.show()
