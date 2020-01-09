685aecff761e1a8ed763663b322f54bee1172f47
faaabi93 <fabian.baiersdoerfer@gmx.de>
2020-01-09 23:17:56 +0100

    added multiseason

--- compare.py
+++ compare.py
@@ -6,46 +6,19 @@ import seaborn as sns
 DATA_DIR = "/Users/fabian.baiersdoerfer/Desktop/ff_data"
 
 results2017 = pd.read_csv(path.join(DATA_DIR, "2017_season_results.csv"), sep=";")
-owner2017 = pd.read_csv(path.join(DATA_DIR, "2017_season_owners.csv"), sep=";")
+results2018 = pd.read_csv(path.join(DATA_DIR, "2018_season_results.csv"), sep=";")
+results = pd.concat([results2017, results2018], sort=False)
 
-results1 = results2017[["Week", "Type", "TeamA", "TeamAScore"]]
-results2 = results2017[["Week", "Type", "TeamB", "TeamBScore"]]
+owners2017 = pd.read_csv(path.join(DATA_DIR, "2017_season_owners.csv"), sep=";")
+owners2018 = pd.read_csv(path.join(DATA_DIR, "2018_season_owners.csv"), sep=";")
+owners2019 = pd.read_csv(path.join(DATA_DIR, "2019_season_owners.csv"), sep=";")
+owners = pd.concat([owners2017, owners2018, owners2019], sort=False)
+
+results1 = results[["Week", "Type", "TeamA", "TeamAScore"]]
+results2 = results[["Week", "Type", "TeamB", "TeamBScore"]]
 
 availableSeasons = [2017, 2018, 2019]
 season = 0
-
-while True:
-  try:
-    season = int(input("Welche Saison möchtest du analysieren? "))
-    if season in availableSeasons:
-      print("Du hast die Saison " + str(season) + " gewählt")
-      break
-    else:
-      print("Diese Saison ist nicht verfügbar!")
-  except ValueError:
-    print("Das ist kein zulässiges Format. Gib einfach nur ein Jahr ein. Beispielsweise '2017'")
-
-print("Welche Teams möchtest du vergleichen?")
-
-a = {}
-k = 0
-if season == 2017:
-  b = 10
-  print("Maximal 10 Teams möglich")
-else:
-  b = 12
-  print("Maximal 12 Teams möglich")
-
-print("Geben Sie die zu vergleichenden Teamnamen ein. stop für break")
-
-while k < b:
-  key = k
-  value = input("Teamnamen für das " + str(k+1) + ". Team eingeben: ")
-  if value == "stop":
-    break
-  else:
-    a[key] = value 
-    k += 1
 
 def winner(row):
     if row["TeamAScore"] > row["TeamBScore"]:
@@ -54,6 +27,7 @@ def winner(row):
         return row["TeamB"]
     else:
         return "draw"
+
 
 def teams(row):
     return [row["TeamA"], row["TeamB"]]
@@ -84,15 +58,50 @@ def get_opponent_score(row):
         return row["score"]
 
 
-# print columns of results2017
+while True:
+    try:
+        season = int(input("Welche Saison möchtest du analysieren? "))
+        if season in availableSeasons:
+            print("Du hast die Saison " + str(season) + " gewählt")
+            break
+        else:
+            print("Diese Saison ist nicht verfügbar!")
+    except ValueError:
+        print("Das ist kein zulässiges Format. Gib einfach nur ein Jahr ein. Beispielsweise '2017'")
 
-results2017["winner"] = results2017.apply(winner, axis=1)
-results2017["margin"] = results2017.apply(win_margin, axis=1)
-results2017["teams"] = results2017.apply(teams, axis=1)
+print("Welche Teams möchtest du vergleichen?")
+
+a = {}
+k = 0
+if season == 2017:
+    b = 10
+    print("Maximal 10 Teams möglich")
+else:
+    b = 12
+    print("Maximal 12 Teams möglich")
+
+print("Geben Sie die zu vergleichenden Teamnamen ein. stop für break")
+
+while k < b:
+    key = k
+    value = input("Teamnamen für das " + str(k + 1) + ". Team eingeben: ")
+    if value == "stop":
+        break
+    elif value == "":
+        break
+    else:
+        a[key] = value
+        k += 1
+
+results_a = results.query("season == @season").copy()
+print(results)
+results_a["winner"] = results_a.apply(winner, axis=1)
+results_a["margin"] = results_a.apply(win_margin, axis=1)
+results_a["teams"] = results_a.apply(teams, axis=1)
 
 # splitting and reducing the df for concatinating later
-teamA = results2017[["Week", "Type", "TeamA", "TeamAScore", "margin", "TeamB", "winner"]]
-teamB = results2017[["Week", "Type", "TeamB", "TeamBScore", "margin", "TeamA", "winner"]]
+teamA = results_a[["Week", "Type", "TeamA", "TeamAScore", "margin", "TeamB", "winner", "season"]]
+teamB = results_a[["Week", "Type", "TeamB", "TeamBScore", "margin", "TeamA", "winner", "season"]]
 
 # renaming the columns to concatenate
 teamA = teamA.rename(columns={"Week": "week", "TeamA": "team", "TeamAScore": "score", "TeamB": "opponent"})
@@ -107,34 +116,32 @@ a_list = list(a.values())
 output_list = []
 
 mean_df = complete.mean()
-print(mean_df)
-mean_score = mean_df.iloc[2]
+mean_score = mean_df.iloc[1]
 
 for i in a_list:
-  output = complete.query("team == @i").copy()
-  output["opponentScore"] = output.apply(get_opponent_score, axis=1)
-  with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    print("Dataframe für das Team von " + i)
-    print(output)
-  output_list.append(output)
+    output = complete.query("team == @i").copy()
+    output["opponentScore"] = output.apply(get_opponent_score, axis=1)
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        print("Dataframe für das Team von " + i)
+        print(output)
+    output_list.append(output)
 
 comparison = pd.concat(output_list, sort=False)
 
 # Array of specific colors I later want to use in the hue in the replot-function
 colors = ["#4374B3", "#FF0B04", "#3CB44B", "#FFE119", "#FF5733", "#911EB4", "#42D4F4",
-"#F032E6", "#BFEF45", "#469990", "#000000"]
+          "#F032E6", "#BFEF45", "#469990", "#000000"]
+# kwargs for vertical line
+pltlines = {"color": "black", "linestyle": "--", "linewidth": 0.75}
+pltmeanline = {"color": "grey", "linestyle": "-", "linewidth": 0.75}
+
 # Setting the custom colorpalette
 sns.set_palette(sns.color_palette(colors))
-
 g = sns.relplot(x='week', y='score', hue="team", kind='line', data=comparison)
-# kwargs for vertical line
-keyword = {"color": "black", "linestyle": "--", "linewidth": 0.75}
-# Creating a vertical line where the playoffs start
-plt.axvline(14, 0, 200, **keyword)
-plt.axhline(100, **keyword)
-plt.axhline(mean_score, **keyword)
-# Set x and y limit to the graph
-g.set(ylim=(0, 200))
-g.set(xlim=(0, 16))
+plt.axvline(14, 0, 200, **pltlines)
+plt.axhline(100, **pltlines)
+plt.axhline(mean_score, **pltmeanline)
+plt.title("Season 2017")
+g.set(ylim=(0, 200), xlim=(0, 16), ylabel="Punkte", xlabel="Woche")
 g.savefig("test.png")
 plt.show()
